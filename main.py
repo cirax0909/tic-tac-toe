8f8170f721d3946f8f60e8ab8d81ed34a2b810b9
phamtich <tichpham1@gmail.com>
2022-08-30 21:42:49 -0400

    2 player version

--- main.py
+++ main.py
@@ -6,31 +6,17 @@ def printBoard(board):
     print(board["1"] + '|' + board["2"] + '|' + board["3"])
 
 
-def player1():
+def player(le):
     while True:
-        player1_move = input("X please choose the position you want for this turn (1 to 9):")
+        player_move = input(f" {le} please choose the position you want for this turn (1 to 9):")
         try:
-            player1_move = int(player1_move)
-            if 9 > player1_move > 0:
-                if theBoard[f'{player1_move}'] == " ":
-                    theBoard[f'{player1_move}'] = "X"
+            player_move = int(player_move)
+            if 9 >= player_move > 0:
+                if theBoard[f'{player_move}'] == " ":
+                    theBoard[f'{player_move}'] = f"{le}"
                     break
-                else:
-                    print("This position is occupied!")
-            else:
-                print("Please enter a number from 1 to 9.")
-        except:
-            print("Please enter a number only!")
-
-
-def player2():
-    while True:
-        player2_move = input("O please choose the position you want for this turn (1 to 9):")
-        try:
-            player2_move = int(player2_move)
-            if 9 >= player2_move > 0:
-                if theBoard[f'{player2_move}'] == " ":
-                    theBoard[f'{player2_move}'] = "O"
+                elif theBoard[f'{player_move}'] == " ":
+                    theBoard[f'{player_move}'] = f"{le}"
                     break
                 else:
                     print("This position is occupied!")
@@ -43,15 +29,21 @@ def player2():
 def check_Winner(le):
     if (theBoard["7"] == le and theBoard["8"] == le and theBoard["9"] == le and le != ' ') or \
             (theBoard["4"] == le and theBoard["5"] == le and theBoard["6"] == le and le != ' ') or \
-        (theBoard["1"] == le and theBoard["2"] == le and theBoard["3"] == le and le != ' ') or \
-        (theBoard["7"] == le and theBoard["4"] == le and theBoard["1"] == le and le != ' ') or \
-        (theBoard["8"] == le and theBoard["5"] == le and theBoard["2"] == le and le != ' ') or\
-        (theBoard["9"] == le and theBoard["6"] == le and theBoard["3"] == le and le != ' ') or \
-        (theBoard["7"] == le and theBoard["5"] == le and theBoard["3"] == le and le != ' ') or \
-        (theBoard["9"] == le and theBoard["5"] == le and theBoard["1"] == le and le != ' '):
+            (theBoard["1"] == le and theBoard["2"] == le and theBoard["3"] == le and le != ' ') or \
+            (theBoard["7"] == le and theBoard["4"] == le and theBoard["1"] == le and le != ' ') or \
+            (theBoard["8"] == le and theBoard["5"] == le and theBoard["2"] == le and le != ' ') or \
+            (theBoard["9"] == le and theBoard["6"] == le and theBoard["3"] == le and le != ' ') or \
+            (theBoard["7"] == le and theBoard["5"] == le and theBoard["3"] == le and le != ' ') or \
+            (theBoard["9"] == le and theBoard["5"] == le and theBoard["1"] == le and le != ' '):
         return le
+
+
+def check_blank():
+    if ' ' not in theBoard.values():
+        print("Draw")
+        return False
     else:
-        return 1
+        return True
 
 
 theBoard = {'7': ' ', '8': ' ', '9': ' ',
@@ -59,14 +51,17 @@ theBoard = {'7': ' ', '8': ' ', '9': ' ',
             '1': ' ', '2': ' ', '3': ' '}
 while True:
     printBoard(theBoard)
-    player1()
-    printBoard(theBoard)
-    player2()
-    if check_Winner("X") == "X":
-        print("Player 1 won.")
-        break
-    elif check_Winner("X") == "O":
-        print("Play 2 won.")
-        break
+    player("X")
+    if check_blank():
+        printBoard(theBoard)
+        if check_Winner("X") == "X":
+            print("Player 1 won.")
+            break
+
+        elif player("O"):
+            if check_Winner("O") == "O":
+                printBoard(theBoard)
+                print("Play 2 won.")
+                break
     else:
-        print("Draw")
+        break
